0b8c64addf463ed3b6d31eebcbd04e77fb44d195
samil isayev <fidanhuseynove04@gmail.com>
2022-10-18 18:29:38 +0400

    added footer

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
 import styles from './Home.module.css';
+import Footer from '../footer/Footer';
 
 const Home = () => {
     return (
         <div className={styles.container}>
             <header>burada header olacaq</header>
             <main>burada movies componenti olacaq</main>
-            <footer>burada footer olacaq</footer>
+            <Footer/>
         </div>
     );
 };
