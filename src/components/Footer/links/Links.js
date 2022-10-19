147219617197e5256fb17af64995437a37ea7f53
samil isayev <fidanhuseynove04@gmail.com>
2022-10-19 17:37:33 +0400

    The end

--- src/components/Footer/links/Links.js
+++ src/components/Footer/links/Links.js
@@ -6,52 +6,52 @@ const Links = () => {
         <div className={styles.links}>
             <div className={styles.linksBloks}>
                 <div>
-                    <h3>Contact with us</h3>
+                    <div className={styles.title}>Contact with us</div>
                     <div className={styles.linksPart}>
-                        <Link to='/f'>+994 50 500 50 50</Link>
-                        <Link to='/f'>+994 12 111 11 11</Link>
-                        <Link to='/f'>infoamchamcsr@gmail.com</Link>
-                        <Link to='/f'>VAT: 319656475 Company no.11843590 Registered in England & Wales</Link>
+                        <Link className={styles.link} to='/f'>+994 50 500 50 50</Link>
+                        <Link className={styles.link} to='/f'>+994 12 111 11 11</Link>
+                        <Link className={styles.link} to='/f'>infoamchamcsr@gmail.com</Link>
+                        <Link className={styles.link} to='/f'>VAT: 319656475 Company no.11843590 Registered in England & Wales</Link>
                     </div>
                 </div>
             </div>
             <div className={styles.linksBloks}>
                 <div>
-                    <h3>About us</h3>
+                    <div className={styles.title}>About us</div>
                     <div className={styles.linksPart}>
-                        <Link to='/f'>About AmCham CSR Platform</Link>
-                        <Link to='/f'>Our team</Link>
+                        <Link className={styles.link} to='/f'>About AmCham CSR Platform</Link>
+                        <Link className={styles.link} to='/f'>Our team</Link>
                     </div>
                 </div>
             </div>
             <div className={styles.linksBloks}>
                 <div>
-                    <h3>Discover</h3>
+                    <div className={styles.title}>Discover</div>
                     <div className={styles.linksPart}>
-                        <Link to='/f'>Projects</Link>
-                        <Link to='/f'>Contributors</Link>
+                        <Link className={styles.link} to='/f'>Projects</Link>
+                        <Link className={styles.link} to='/f'>Contributors</Link>
                     </div>
                 </div>
             </div>
             <div className={styles.linksBloks}>
                 <div>
-                    <h3>Other links</h3>
+                    <div className={styles.title}>Other links</div>
                     <div className={styles.linksPart}>
-                        <Link to='/f'>News</Link>
-                        <Link to='/f'>Partners</Link>
-                        <Link to='/f'>Video tutorial</Link>
-                        <Link to='/f'>AmCham website</Link>
+                        <Link className={styles.link} to='/f'>News</Link>
+                        <Link className={styles.link} to='/f'>Partners</Link>
+                        <Link className={styles.link} to='/f'>Video tutorial</Link>
+                        <Link className={styles.link} to='/f'>AmCham website</Link>
                     </div>
                 </div>
             </div>
             <div className={styles.linksBloks}>
                 <div>
-                    <h3>Policies</h3>
+                    <div className={styles.title}>Policies</div>
                     <div className={styles.linksPart}>
-                        <Link to='/f'>Community guidlines</Link>
-                        <Link to='/f'>Terms of use</Link>
-                        <Link to='/f'>Privacy Policy</Link>
-                        <Link to='/f'>Legal</Link>
+                        <Link className={styles.link} to='/f'>Community guidlines</Link>
+                        <Link className={styles.link} to='/f'>Terms of use</Link>
+                        <Link className={styles.link} to='/f'>Privacy Policy</Link>
+                        <Link className={styles.link} to='/f'>Legal</Link>
                     </div>
                 </div>
             </div>
--- src/components/Footer/subscribe/SubsribeUs.js
+++ src/components/Footer/subscribe/SubsribeUs.js
@@ -5,7 +5,7 @@ const SubscribeUs = () => {
     return (
         <div className={styles.inputAreaContainer}>
             <HeadIcon />
-            <h2 className={styles.h2}>Stay with us</h2>
+            <div className={styles.title}>Stay with us</div>
             <div className={styles.inputArea}>
                 <input className={styles.input} placeholder='Enter your email' />
                 <button className={styles.btn}>Subscribe</button>
